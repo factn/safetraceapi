ef57d999f28f375bf839b5c2cff09ecdb5349f3b
Andres Gomez <andresgomezjr89@gmail.com>
2020-03-20 18:18:11 -0400

    cleared repo

--- RestAPI/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const requestHandler = require('./app');
-// use 3000 as default port
-const port = process.env.PORT || 3000; 
-
-const server = http.createServer (requestHandler);
-
-server.listen(port);
