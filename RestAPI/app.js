f88b317a7a10a31c28e6af2f41f398a24b31a664
Andres Gomez <andresgomezjr89@gmail.com>
2020-03-20 19:19:39 -0400

    added error handling and logging

--- RestAPI/app.js
+++ RestAPI/app.js
@@ -2,7 +2,13 @@ const express = require('express');
 
 const app = express();
 
+//logging
+const morgan = require ('morgan');
+
 const mockDataRoutes = require('./routes/mockData');
+
+// log before using requests
+app.use(morgan('dev'));
 
 //sets up middleware
 app.use('/mockData', mockDataRoutes);
@@ -10,4 +16,23 @@ app.use('/mockData', mockDataRoutes);
 // const otherRoute = require('./routes/otherRoute');
 // app.use('/otherRoute', otherRoute);
 
+
+// handle errors (if we reached this line, we havent reached a suitable route)
+app.use ( (request, response, next) => {
+    const error = new Error ('Not Found!');
+    error.status(404);
+
+    //forward the error request
+    next (error);
+});
+
+// catches any errors
+app.use ( (error, request, response, next) => {
+    response.status(error.status || 500);
+    response.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 module.exports = app;
