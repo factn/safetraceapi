1bfe6bc62a76834ba263857a8396d6150f129176
Andres Gomez <andresgomezjr89@gmail.com>
2020-03-31 23:40:40 -0400

    fixed trycatch

--- sandbox/run-sandbox.js
+++ sandbox/run-sandbox.js
@@ -76,59 +76,65 @@ async function runSandbox (req, res, next) {
         );
 
         cp.on('close', async (code) => {
-            console.log(`Computation Process Exited With Code: ${code}`);
-            
-            function writeLogsSection (section, obj) {
-                logStream.write(`[SANDBOX ${section}]: ===================================================\n\n`);
-                if (obj) logStream.write(obj);
-                logStream.write('\n=====================================================================\n\n');
-            }
-            writeLogsSection ('ERRORS', err)
-            writeLogsSection ('STDOUT', stdOut)
-            writeLogsSection ('STDERR', stdErr)
-            logStream.end();
+            try {
 
-            logStream = resultStream = null;
-            
-            if (err) console.log('Sandbox Errors: ' + err);
-            
-            // archive results
-            console.log('Archiving Results...');
-            let archivePath = resultsPrefix + '_ComputationResults.zip';
-            
-            console.log('Creating Archive Stream...');
-            let outputStream = fs.createWriteStream(archivePath);
-            
-            console.log('Creating Archiver...');
-            var archive = archiver('zip', {
-                zlib: { level: 9 } // Sets the compression level.
-            });
-            
-            // pipe archive data to the file
-            archive.pipe(outputStream);
-            
-            // append a file
-            function appendFile (path, name) {
-                console.log('Archiving: "' + path +  '" TO "' + name + '...');
-                archive.file(path, { name: name });
+                console.log(`Computation Process Exited With Code: ${code}`);
+                
+                function writeLogsSection (section, obj) {
+                    logStream.write(`[SANDBOX ${section}]: ===================================================\n\n`);
+                    if (obj) logStream.write(obj.toString());
+                    logStream.write('\n=====================================================================\n\n');
+                }
+                writeLogsSection ('ERRORS', err);
+                writeLogsSection ('STDOUT', stdOut);
+                writeLogsSection ('STDERR', stdErr);
+                console.log('Dont Writing Logs, Ending Stream...');
+                logStream.end();
+                logStream = resultStream = null;
+                
+                if (err) console.log('Sandbox Errors: ' + err);
+                
+                // archive results
+                console.log('Archiving Results...');
+                let archivePath = resultsPrefix + '_ComputationResults.zip';
+                
+                console.log('Creating Archive Stream...');
+                let outputStream = fs.createWriteStream(archivePath);
+                
+                console.log('Creating Archiver...');
+                var archive = archiver('zip', {
+                    zlib: { level: 9 } // Sets the compression level.
+                });
+                
+                // pipe archive data to the file
+                archive.pipe(outputStream);
+                
+                // append a file
+                function appendFile (path, name) {
+                    console.log('Archiving: "' + path +  '" TO "' + name + '...');
+                    archive.file(path, { name: name });
+                }
+                appendFile(resultsPath, resultsName);
+                appendFile(logsPath, logsName);
+                
+                // handle errors
+                archive.on('warning', onFinished('Archiver Warning'));
+                archive.on('error', onFinished('Archiver Error'));
+                // listen for all archive data to be written 'close' event is fired only when a file descriptor is involved
+                outputStream.on('close', function() {
+                    console.log('Finished Archiving, Size (bytes): ' + archive.pointer());
+                    // send archive file as download response
+                    res.download(archivePath, archivePath.split('/').pop(), onFinished('SUCCESS'));
+                });
+                
+                console.log('Done Archiving...');
+                // finalize the archive (ie we are done appending files but streams have to finish yet)
+                // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
+                archive.finalize();
             }
-            appendFile(resultsPath, resultsName);
-            appendFile(logsPath, logsName);
-            
-            // handle errors
-            archive.on('warning', onFinished('Archiver Warning'));
-            archive.on('error', onFinished('Archiver Error'));
-            // listen for all archive data to be written 'close' event is fired only when a file descriptor is involved
-            outputStream.on('close', function() {
-                console.log('Finished Archiving, Size (bytes): ' + archive.pointer());
-                // send archive file as download response
-                res.download(archivePath, archivePath.split('/').pop(), onFinished('SUCCESS'));
-            });
-            
-            console.log('Done Archiving...');
-            // finalize the archive (ie we are done appending files but streams have to finish yet)
-            // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
-            archive.finalize();
+            catch (e) {
+                onFinished('Caught After Proccess Close')(e);
+            }
         });
     }
     catch (err) {     
