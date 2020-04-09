bb9ee05cffb78f354078d97c4d5d10ec8b1d70e7
Priyank Patel <priyank.patel@stackspace.ca>
2020-04-09 04:28:51 -0400

    Fix HomeScreen layout

---
+++ SafeTrace_App/SafeTrace/screens/HomeScreen/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components/native";
+
+export const BaseLayout = styled.View`
+  flex: 1;
+  background-color: #FFFFFF;
+`;
+
+export const InputContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-horizontal: 15;
+`;
+
+export const Input = styled.TextInput`
+  width: 50%;
+  border-bottom-color: #000000;
+  border-bottom-width: 1px;
+`;
+
+export const Button = styled.Button`
+  
+`;
