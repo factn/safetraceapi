0299a7a62a722a7ee2d0b3f60f087ccced85d385
Priyank Patel <priyank.patel@stackspace.ca>
2020-04-09 09:53:45 -0400

    Add Links Screen

---
+++ SafeTrace_App/SafeTrace/screens/LinksScreen/index.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { WebBrowser } from "expo";
+
+import {
+  BaseLayout,
+  OptionButtonContainer,
+  OptionIconContainer,
+  OptionLabelContainer,
+  OptionLabel
+} from "./styles";
+
+interface IOptionButtonProps {
+  icon: string;
+  label: string;
+  onPress: any;
+  isLastOption?: boolean;
+}
+
+const OptionButton = ({ icon, label, onPress, isLastOption }: IOptionButtonProps) => {
+  return (
+    <OptionButtonContainer lastOption={isLastOption} onPress={onPress}>
+      <View style={{ flexDirection: "row" }}>
+        <OptionIconContainer>
+          <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
+        </OptionIconContainer>
+        <OptionLabelContainer>
+          <OptionLabel>{label}</OptionLabel>
+        </OptionLabelContainer>
+      </View>
+    </OptionButtonContainer>
+  );
+}
+
+const LinksScreen = () => {
+  return (
+    <BaseLayout
+      contentContainerStyle={{ paddingTop: 15 }}
+    >
+      <OptionButton
+        icon="md-school"
+        label="Read the Expo documentation"
+        onPress={() => WebBrowser.openBrowserAsync("https://safetraceapi.org")}
+      />
+
+      <OptionButton
+        icon="md-compass"
+        label="Read the React Navigation documentation"
+        onPress={() =>
+          WebBrowser.openBrowserAsync("https://reactnavigation.org")
+        }
+      />
+
+      <OptionButton
+        icon="ios-chatboxes"
+        label="Ask a question on the forums"
+        onPress={() => WebBrowser.openBrowserAsync("https://forums.expo.io")}
+        isLastOption
+      />
+    </BaseLayout>
+  );
+};
+
+LinksScreen.navigationOptions = {
+  header: null,
+};
+
+export default LinksScreen;
