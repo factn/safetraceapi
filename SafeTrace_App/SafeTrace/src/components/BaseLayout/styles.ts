e3675238cd8999de999a103a000f026a9cdac7fe
Priyank Patel <priyank.patel@stackspace.ca>
2020-04-23 18:50:00 -0400

    Add baselayout styling

--- SafeTrace_App/SafeTrace/src/components/BaseLayout/styles.ts
+++ SafeTrace_App/SafeTrace/src/components/BaseLayout/styles.ts
@@ -1,7 +1,9 @@
 import styled from 'styled-components/native';
-import { StatusBar } from 'react-native';
+import { StatusBar, ITheme, Platform } from 'react-native';
 
-export const RootContainer = styled.View`
+export const RootContainer = styled.View<{
+  theme?: ITheme;
+}>`
   flex: 1;
-  padding-top: ${StatusBar.currentHeight}%;
+  ${props => Platform.OS === 'android' && `padding-top: ${StatusBar.currentHeight}`};
 `;
